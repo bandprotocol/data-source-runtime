86c697a9972d3fe04e86a6005b14732ab935f1ee
Nattapat Iammelap <nattapat@Nattapats-MacBook-Pro.local>
2025-07-21 16:16:40 +0700

    update version

--- lambda/pack.sh
+++ lambda/pack.sh
@@ -7,7 +7,7 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-cd venv/lib/python3.9/site-packages
+cd venv/lib/python3.13/site-packages
 zip -r9 ${OLDPWD}/lambda-executor.zip .
 
 cd ${OLDPWD}
